0606673bab9be62813bb7e35f02dc0f0c40f6543
Daniel Boon <danielboon93@outlook.com>
2014-11-19 21:50:03 +0000

    Skeleton SDL for building a text editor
    Includes some basic sdl files Sam and I wrote last week.
    
    At the moment it simply creates a blank window titled with a name
    specified in text_editor.c

---
+++ text_editor/artc_sdl2.h
@@ -0,0 +1,17 @@
+#include "SDL.h"
+
+#define WIN_WIDTH 1024		
+#define WIN_HEIGHT 768
+
+#define SDL_8BITCOLOUR 256
+
+struct SDL_Win {
+   SDL_bool finished;
+   SDL_Window *win;
+   SDL_Renderer *renderer;
+};
+typedef struct SDL_Win SDL_Win;
+
+void SDL_Win_Init(SDL_Win *w, char win_name[20]);
+void SDL_Events(SDL_Win *w);
+void SDL_SetDrawColour(SDL_Win *w, Uint8 r, Uint8 g, Uint8 b);
---
+++ text_editor/text_editor.c
@@ -0,0 +1,24 @@
+#include "artc_sdl2.h"
+
+int main(void) {
+	SDL_Win w;
+	SDL_Win_Init(&w, "ArtC Text Editor");
+	SDL_Rect text_editor;
+	text_editor.w = WIN_WIDTH;
+    text_editor.h = WIN_HEIGHT;
+	SDL_SetTextInputRect(&text_editor);
+	SDL_StartTextInput();
+
+	do { 
+		
+		SDL_SetDrawColour(&w, 255, 255, 255);
+    	text_editor.x = (0);
+    	text_editor.y = (0);
+		SDL_RenderFillRect(w.renderer, &text_editor); 
+	    SDL_RenderPresent(w.renderer);
+	    SDL_UpdateWindowSurface(w.win); 
+		SDL_Events(&w);	
+	}while(!w.finished);
+	SDL_StopTextInput();
+	atexit(SDL_Quit);
+}
