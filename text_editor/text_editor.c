136bd638688610313922e83a0f4259d4fe5630c6
Daniel Boon <danielboon93@outlook.com>
2014-11-20 16:54:37 +0000

    Cleaned up text_editor file

--- text_editor/text_editor.c
+++ text_editor/text_editor.c
@@ -1,31 +1,31 @@
 #include "../artc_sdl2.h"
 
 SDL_Texture* SurfaceToTexture(SDL_Surface* surface, SDL_Win* w);
+void CreateTextTextures();
 
 int main(void) {
 	SDL_Win w;
-	SDL_Surface display;
 	SDL_Win_Init(&w, "ARTC Text Editor");
-	
-	SDL_TTF_Init();
-  	TTF_Font *font;
-	
-    font = TTF_OpenFont("font/FreeSans.ttf", 200);	
-	SDL_Color text_colour = { 255, 255, 0, 255 }; // white
 
+	//Initialise text things
+	SDL_TTF_Init();
+    TTF_Font* font = SDL_Load_Font("font/FreeSans.ttf", 90);
+
+	SDL_Color text_colour = {255, 255, 255, 255}; // white
+
+	//Create rect
 	SDL_Rect solid_rect;
-	solid_rect.h = 500;
-	solid_rect.w = 500;
+	solid_rect.h = 20;
+	solid_rect.w = 20;
 	
+	//Create text texture
 	SDL_Surface* solid_text = TTF_RenderText_Solid(font, "Welcome", text_colour);
 	SDL_Texture *solid_texture = SurfaceToTexture(solid_text, &w);
-	
 	SDL_QueryTexture(solid_texture, NULL, NULL, &solid_rect.w, &solid_rect.h);
 	solid_rect.x = 0;
 	solid_rect.y = 0;
- 	
+
 	do {		 
-		//Blank screen
 		SDL_RenderClear(w.renderer);	
 		SDL_RenderCopy(w.renderer, solid_texture, NULL, &solid_rect);
 		SDL_RenderPresent(w.renderer);
