92bd6ddaca2b541775531dad9531140531a61861
sm1313 <sm1313@my.bristol.ac.uk>
2014-11-20 17:32:29 +0000

    Tidying
    Rect 'drawbutton' seems to take up the whole page, can't figure out why.
    Probably because we're pissing about with textures and have no clue what
    we're doing.

--- interface.c
+++ interface.c
@@ -1,48 +1,31 @@
 #include "artc_sdl2.h"
 
+void make_rect(SDL_Win *win, SDL_Rect *rect, 
+                int x, int y, int w, int h, 
+                int r, int g, int b);
 int main()
 {
     SDL_Win win;
     SDL_Win_Init(&win, "Art-C interface");
     SDL_TTF_Init();
     TTF_Font *font = SDL_Load_Font("font/FreeSans.ttf", 25);
-
+    
     SDL_Rect menubar;
-    menubar.x = 0;
-    menubar.y = 0;
-    menubar.w = WIN_WIDTH;
-    menubar.h = 50;
-    SDL_SetRenderDrawColor(win.renderer, 255,128,128,255);
-    SDL_RenderFillRect(win.renderer, &menubar);
+    make_rect(&win, &menubar, 0, 0, WIN_WIDTH, 50, 255, 64, 64);
 
     SDL_Rect texteditor;
-    texteditor.x = 0;
-    texteditor.y = menubar.h;
-    texteditor.w = WIN_WIDTH/2;
-    texteditor.h = WIN_HEIGHT-menubar.h-5-25; 
-        // border of 5, button 25
-    SDL_SetRenderDrawColor(win.renderer, 128,128,128,255);
-    SDL_RenderFillRect(win.renderer, &texteditor);
+    make_rect(&win, &texteditor, 0, menubar.h, WIN_WIDTH/2, WIN_HEIGHT-menubar.h-30, 128, 128, 128);
+    // border of 5, button 25
 
     SDL_Rect canvas;
-    canvas.x = texteditor.w;
-    canvas.y = menubar.h;
-    canvas.w = texteditor.w;
-    canvas.h = WIN_HEIGHT-menubar.h-5-25;
-    SDL_SetRenderDrawColor(win.renderer, 255,255,255,255);
-    SDL_RenderFillRect(win.renderer, &canvas);
+    make_rect(&win, &canvas, texteditor.w, menubar.h, texteditor.w, WIN_HEIGHT-menubar.h-30, 255, 255, 255);
 
     SDL_Rect drawbutton;
-    drawbutton.w = 100;
-    drawbutton.h = 25;
-    drawbutton.x = texteditor.w - drawbutton.w - 5;
-    drawbutton.y = WIN_HEIGHT - drawbutton.h - 5;
-    SDL_SetRenderDrawColor(win.renderer, 255,64,64,255);
-    SDL_RenderFillRect(win.renderer, &drawbutton);
+    make_rect(&win, &drawbutton, 100, 25, texteditor.w-drawbutton.w-5, WIN_HEIGHT-drawbutton.h-5, 255, 0, 0);
 
-    SDL_Color generatecolour = {64,255,64,255};
-    SDL_Surface* generate = TTF_RenderText_Solid(font, "GENERATE", generatecolour);
-    SDL_Texture* solid_texture = SurfaceToTexture(generate, &win);
+    SDL_Color textcolour = {64,255,64,255};
+    SDL_Surface* text = TTF_RenderText_Solid(font, "GENERATE", textcolour);
+    SDL_Texture* solid_texture = SurfaceToTexture(text, &win);
     SDL_QueryTexture(solid_texture, NULL, NULL, &drawbutton.w, &drawbutton.h);
 
     SDL_RenderClear(win.renderer);
@@ -50,9 +33,22 @@ int main()
     SDL_RenderPresent(win.renderer);
     SDL_UpdateWindowSurface(win.win);
 
-    // Pass texteditor rect to texteditor module.
+    // Pass texteditor rect to texteditor module so it knows where it is.
+
     while(!win.finished) {
         SDL_Events(&win);
     }
     return 0;
+}
+
+void make_rect(SDL_Win *win, SDL_Rect *rect, 
+                int x, int y, int w, int h, 
+                int r, int g, int b)
+{
+  rect->x = x;
+  rect->y = y;
+  rect->w = w;
+  rect->h = h;
+  SDL_SetRenderDrawColor(win->renderer, r, g, b,255);
+  SDL_RenderFillRect(win->renderer, rect);
 }
