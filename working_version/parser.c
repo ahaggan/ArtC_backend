61e3da2898d2c7f5702da957f94385a08ef5788a
Ashley <ashleyhaggan@gmail.com>
2015-01-05 12:02:10 +0000

    The return values from the parser are now used, will look at writing some automated tests to check these. This should also stop the program closing if the users code is wrong - parser will return FALSE.

--- working_version/parser.c
+++ working_version/parser.c
@@ -1,31 +1,30 @@
+//  This module tests the users program for gramatical accuracy. It will return an error if the input is incorrect. 
+//
+//  As the program is tested, the correct attributes are assigned to an interpreter array. If the program is written correctly
+//  this array is passed to the intereter module which will assign values from the user to the given fractal
 
 #include "parser.h"
-
-/* parser should be passed a text file 
-   also, it should treat \newlines as spaces (try ending the program with a } only one line below)*/
+//int main(Draw *fractal, char *file_name){     //Use this to pass a file name to the parser function.
 int parser(Draw *fractal){
-    int i; //what is 'i'?
-    FILE *file_pointer; // give this a proper name
+    //test_parser();
+    int i;
+    FILE *file_pointer;
     Prog program;
+    
+    
     
     program.interpreter_index = 0;
     program.fractal = fractal;
     
     initialise_words_array(&program);
-
+    
     create_struct_array(program.actions);
     
-    //make_default(&program);    
-    //printf("\nCurrent word: %d\n", program.current_word);
-    //interpreter(&program);
-    //printf("\nParsed OK!\n");
-    //test_interpreter(&program);
-    //initialise_words_array(&program);
-
+    
     initialise_interpreter_array(&program);
-    if((file_pointer = fopen("user_code.artc", "r")) == NULL){
+    if((file_pointer = fopen("parser.txt", "r")) == NULL){
         fprintf(stderr, "\nCannot open file\n");
-        exit(2);
+        return FALSE;
     }
     
     i = 0;
@@ -34,13 +33,18 @@ int parser(Draw *fractal){
         //printf("\nWord read: %s\n", program.words[i]);
         i++;
     }
-    validate(&program);
+    
+    if (validate(&program) == FALSE){
+        return FALSE;
+    }
+    
     printf("\nParsed OK!\n");
     interpreter(&program);
     test_interpreter(&program);
+    return TRUE;
 }
 
-void initialise_words_array(Prog* program){
+void initialise_words_array(Prog *program){
     int i;
     program->current_word = 0;
     for(i = 0; i < MAX_WORDS; i++){
@@ -48,7 +52,7 @@ void initialise_words_array(Prog* program){
     }
 }
 
-void initialise_interpreter_array(Prog* program){
+void initialise_interpreter_array(Prog *program){
     int i;
     
     for(i = 0; i < NO_OF_INSTRUCTIONS; i++){
@@ -56,7 +60,7 @@ void initialise_interpreter_array(Prog* program){
     }
 }
 
-void test_interpreter(Prog* program){
+void test_interpreter(Prog *program){
      printf("\nInside test\n");
     for(int i = 0; i < NO_OF_INSTRUCTIONS; i++){
     
@@ -65,13 +69,13 @@ void test_interpreter(Prog* program){
     }
     for(int i = 0; i < MAX_ITERATIONS; i++){
     
-        printf("\nFractal colour %d: %s", i, program->fractal->colour[i]);
-        printf("\nFractal shape %d: %s", i, program->fractal->shape[i]);
-        printf("\nFractal type %d: %s", i, program->fractal->type[i]);
+        printf("\nFactal colour %d: %s", i, program->fractal->colour[i]);
+        printf("\nFactal shape %d: %s", i, program->fractal->shape[i]);
+        printf("\nFactal type %d: %s", i, program->fractal->type[i]);
     }
 }
 
-void make_default(Prog* program){
+void make_default(Prog *program){
     FILE *file_pointer;
     int i;
     if((file_pointer = fopen("default.txt", "r")) == NULL){
@@ -89,38 +93,45 @@ void make_default(Prog* program){
 }
     
 
-int validate(Prog* program){
+int validate(Prog *program){
     
     if(!strings_match(program->words[program->current_word], "run{")){
         printf("\nProgram needs to start with the word 'run{'");
-        exit(1);
+        return FALSE;
     }
     program->current_word += 1;
-    funclist(program);
+    if(funclist(program) == TRUE){
+        return TRUE;
+    }
+    return FALSE;
     
 }
 
-int funclist(Prog* program){
+int funclist(Prog *program){
     if(strings_match(program->words[program->current_word], "}")){
         strcpy(program->interpreter[program->interpreter_index], "end" );
         program->interpreter_index += 1;
         return TRUE;
     }
-    else if(strings_match(program->words[program->current_word], "")){
+    else if(strings_match(program->words[program->current_word], "")){      //Should this be something else, EOF?
         fprintf(stderr, "\nProgram needs to end with '}'.\n");
-        exit(2);
+        return FALSE;
     }
-    function(program);
-    program->current_word += 1;
-    funclist(program);
+    if(function(program)){
+        program->current_word += 1;
+        return funclist(program);
+    }
+    return FALSE;
 }
 
-int function(Prog* program){
+int function(Prog *program){
     if(strings_match(program->words[program->current_word], "if")){
         strcpy(program->interpreter[program->interpreter_index], "conditional");
             program->current_word += 1;
         if(check_if(program->words[program->current_word]) == TRUE){
-            statement(program);
+            if (!statement(program)){
+                return FALSE;
+            } 
             program->current_word -= 1;
             program->interpreter_index += 1;
             strcpy(program->interpreter[program->interpreter_index], program->words[program->current_word]);
@@ -128,54 +139,56 @@ int function(Prog* program){
             program->current_word += 1;
             strcpy(program->interpreter[program->interpreter_index], program->words[program->current_word]);
             program->interpreter_index += 1;
-            conditional(program);
+            return conditional(program);
         }
+        return FALSE;
         
     }
     else if(strings_match(program->words[program->current_word], "for")){
         program->current_word += 1;
-        loop(program);
+        return loop(program);
     }
     else{
-        statement(program);
+        return statement(program);
     }
 }
 
-int loop(Prog* program){  
+int loop(Prog *program){  
     int start_iteration, end_iteration;
     if(!strings_match(program->words[program->current_word], "iterations")){
             fprintf(stderr, "\nFor loop only works for iterations\n");
-            exit(1);
+            return FALSE;
     }
     statement(program);
     start_iteration = atoi(program->words[program->current_word]);
     program->current_word += 1;
     if(!strings_match(program->words[program->current_word], "to")){
             fprintf(stderr, "\nFor loop needs the word 'to' between the two conditions\n");
-            exit(1);
+            return FALSE;
     }
     program->current_word += 1;
     if(!strings_match(program->words[program->current_word], "iterations")){
             fprintf(stderr, "\nFor loop only works for iterations\n");
-            exit(1);
+            return FALSE;
     }
     statement(program);
     end_iteration = atoi(program->words[program->current_word]);
     program->current_word += 1;
     if(!strings_match(program->words[program->current_word], "{")){
             fprintf(stderr, "\nFor loop starts with '{'\n");
-            exit(1);
+            return FALSE;
     }
     program->current_word += 1;
     if(start_iteration > end_iteration){
         fprintf(stderr, "\nStart iteration needs to be lower than the end iteration in your for loop.\n");
-        exit(1);
+        return FALSE;
     }
     
     for_loop(program, start_iteration, end_iteration);
+    return TRUE;
 }
 
-void for_loop(Prog* program, int start, int end){
+void for_loop(Prog *program, int start, int end){
     int start_counter = program->current_word;
     int i;
     for(i = start; i <= end; i++){
@@ -192,32 +205,32 @@ void for_loop(Prog* program, int start, int end){
 }
        
         
-int conditional(Prog* program){  //if function
+int conditional(Prog *program){  //if function
     //statement(program);
     program->current_word += 1;
     if(!strings_match(program->words[program->current_word], "then{")){
         printf("\nIf condition needs to be followed with 'then{'");
-        exit(1);
+        return FALSE;
     }
     program->current_word += 1;
-    funclist(program);
+    return funclist(program);
     
 }
 
-int statement(Prog* program){
+int statement(Prog *program){
     if (check_action(program->words[program->current_word]) == TRUE){
         program->current_word += 1;
         if(attribute(program)){
-            //assign_to_fractal(
+            return TRUE;
         }
         else{
             fprintf(stderr, "\n Attribute word %s is not valid with action %s", program->words[program->current_word], program->words[program->current_word-1] );
-            exit(2);
+            return FALSE;
         }
     }
     else{
         fprintf(stderr, "\n Action word %s is not valid.", program->words[program->current_word]);
-        exit(2);
+        return FALSE;
     }
 }
 
@@ -232,7 +245,7 @@ int check_action(char *word){
     return FALSE; 
 }
 
-int attribute(Prog* program){
+int attribute(Prog *program){
     int i, j;
     for(i = 0; i < ACTION_SIZE; i++){
         if(strings_match(program->actions[i].name, program->words[program->current_word-1])){
@@ -291,7 +304,6 @@ int check_if(char *word){
         return TRUE;
     }
     fprintf(stderr, "\nThe action %s is not a valid condition for an if statement.\n", word);
-    exit(1);
     return FALSE;
 }
 
@@ -322,59 +334,3 @@ void create_struct_array(Action *actions){
 		array_cnt += 1;
 	}
 }
-
-/* What is this? */
-/*
-void assign_value(Prog* program, action_word i){
-
-	switch(i){
-		case colour:
-           for(int j=0; j<MAX_ITERATIONS; j++) {
-                    program->fractal->colour[j] = (char*)malloc(strlen(program->words[program->current_word])*sizeof(char));
-	    		    strcpy(program->fractal->colour[j], program->words[program->current_word]);
-            }
-			break;
-		case move:
-			program->fractal->move = (char*)malloc(strlen(program->words[program->current_word])*sizeof(char));
-			strcpy(program->fractal->move, program->words[program->current_word]);
-			break;
-		case size:
-			program->fractal->size = atoi(program->words[program->current_word]);
-        	program->fractal->startx -= (size/2);
-	        program->fractal->starty -= (size/2);
-            // In order to centre the image properly.
-//   HAVE NOT GOT TIME TO MAKE WORK PROPERLY RIGHT NOW WILL FIX LATER PROBABLY.
-			break;
-		case shape:
-		    for(int j=0; j<MAX_ITERATIONS; j++) {
-			    program->fractal->shape[j] = (char*)malloc(strlen(program->words[program->current_word])*sizeof(char));
-			    strcpy(program->fractal->shape[j], program->words[program->current_word]);
-			}
-			break;
-			
-		case startx:
-		    program->fractal->startx = atoi(program->words[program->current_word]);
-		    break;
-		case starty:
-		    program->fractal->starty = atoi(program->words[program->current_word]);
-		    break; 
-		case endx:
-		    program->fractal->endx = atoi(program->words[program->current_word]);
-		    break; 
-		case endy:
-		    program->fractal->endy = atoi(program->words[program->current_word]);
-		    break;  
-		case type:
-		    for(int j=0; j<MAX_ITERATIONS; j++) {
-		        program->fractal->type[j] = (char*)malloc(strlen(program->words[program->current_word])*sizeof(char));
-			    strcpy(program->fractal->type[j], program->words[program->current_word]);
-			}
-			break;		
-        case iterations:
-            program->fractal->iterations = atoi(program->words[program->current_word]);
-            break;
-	}
-	
-}
-*/
-
