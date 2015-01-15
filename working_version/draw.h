634904021ce96bcee8ec450a372dafd8091c3df1
sm1313 <sm1313@my.bristol.ac.uk>
2015-01-15 15:59:07 +0000

    Random colour is better

--- working_version/draw.h
+++ working_version/draw.h
@@ -1,6 +1,8 @@
 #include "display.h"
 #include <stdlib.h>
 
+#define COLOUR_SIZE 17
+
 void draw_sdl(Interface interface, Draw *fractal, int x, int y, int size, float angle, int i);
 
 void SDL_Line(SDL_Renderer* renderer, int xs, int ys, int xe, int ye, int thickness, float angle);
